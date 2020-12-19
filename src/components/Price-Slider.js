bb80f795ced4b0b5276fc076d78ec3e585cf5095
pr <pjr94@users.noreply.github.com>
2020-12-19 15:07:11 +0000

    v1.2

--- src/components/Price-Slider.js
+++ src/components/Price-Slider.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-import Slider from "rc-slider";
+//import Slider, { Range } from "rc-slider";
 import "rc-slider/assets/index.css";
 import Collapsible from "react-collapsible";
 // https://www.npmjs.com/package/react-collapsible
 
+const Slider = require('rc-slider');
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
 //const Handle = Slider.Handle;
@@ -13,11 +14,21 @@ export default function PriceSlider({ priceState, setPriceState }) {
   const handleRangeChange = (val) => {
     setPriceState(val);
   };
-
+  /*
+<Range
+          min={0}
+          max={1000}
+          defaultValue={priceState}
+          tipFormatter={(x) => `${x}`}
+          step={50}
+          onAfterChange={handleRangeChange}
+          className="range-slider"
+        />
+        */
   return (
     <div className="filters">
       <Collapsible trigger="Price Range" open="true">
-        <Range
+      <Range
           min={0}
           max={1000}
           defaultValue={priceState}
