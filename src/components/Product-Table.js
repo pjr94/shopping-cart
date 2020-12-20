7ecd3e446852d23fea63a1fa935636e7b49ab024
pr <pjr94@users.noreply.github.com>
2020-12-20 13:07:55 +0000

    v1.3

--- src/components/Product-Table.js
+++ src/components/Product-Table.js
@@ -54,6 +54,8 @@ export default function ProductTable({
               filteredProducts.push(products[j]);
             }
             break;
+            
+          default:// None
         }
       }
     }
